203f707e85021de2510f75f2e7667b6aabf76095
Udit Kapahi <ukitkk@gmail.com>
2016-08-30 17:37:10 +0530

    Add & Delete Comments

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,20 @@
+class CommentsController < ApplicationController
+
+  def create
+    @post=Post.find params[:post_id]
+    @comment=@post.comments.create(params[:comment].permit(:name, :body))
+    redirect_to post_path(@post)
+  end
+
+  def destroy
+    @post=Post.find params[:post_id]
+    @comment=@post.comments.find(params[:id])
+    @comment.destroy
+    redirect_to post_path(@post)
+  end
+
+  private
+  def comments_params
+    params.require(:comment).permit(:name, :body)
+  end
+end
