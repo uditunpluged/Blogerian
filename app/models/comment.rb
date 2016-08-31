bf2bfd1a2333c72b9bc084a0be3bdb6666c4f9c3
Udit Kapahi <ukitkk@gmail.com>
2016-08-31 12:59:01 +0530

    Added users,signin,registration and other functionalities.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
+  validates :name, presence: true
+  validates :body, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   # resources :posts
   # IN ORDER TO NEST COMMENTS ROUTES INSIDE THE POST ROUTES , DO FOLLOWING AND COMMENT ABOVE ONE
   resources :posts do
